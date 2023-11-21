e6d2a0905f03d709cb454eb6d80c112956bc4680
Mike <mike@mike-francis.org>
2023-11-21 12:31:23 +0000

    updates to task 4 based on feedback from Jonathan

--- api/controllers/articles-controllers.js
+++ api/controllers/articles-controllers.js
@@ -4,7 +4,6 @@ exports.getArticleById = (req, res, next) => {
   const { article_id } = req.params;
   selectArticleById(article_id)
     .then((article) => {
-      console.log({article});
       res.status(200).send({ article });
     })
     .catch(next);
