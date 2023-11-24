656d8fd9d89063030d4ff9402f649254993a081a
Mike <mike@mike-francis.org>
2023-11-24 17:06:16 +0000

    18 - completes advanced PATCH /api/comments/:comment_id function

--- api/models/comments-models.js
+++ api/models/comments-models.js
@@ -49,3 +49,17 @@ exports.removeCommentById = (comment_id) => {
       }
     });
 };
+
+exports.updateCommentById = (comment_id, inc_votes) => {
+  return db
+    .query(
+      `UPDATE comments SET votes = votes + $1 WHERE comment_id = $2 RETURNING *;`,
+      [inc_votes, comment_id]
+    )
+    .then(({ rows }) => {
+      if (!rows.length) {
+        return Promise.reject({ status: 404, msg: "not found" });
+      }
+      return rows[0];
+    });
+};
--- routes/comments-router.js
+++ routes/comments-router.js
@@ -1,9 +1,12 @@
 const {
   deleteCommentById,
+  patchCommentById,
 } = require("../api/controllers/comments-controllers");
 
 const commentsRouter = require("express").Router();
 
 commentsRouter.delete("/:comment_id", deleteCommentById);
+
+commentsRouter.patch("/:comment_id", patchCommentById);
 
 module.exports = commentsRouter;
