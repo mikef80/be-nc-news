ff954469f976cf219b25f4ccb5ae5fd7ca4c8c9e
Mike <mike@mike-francis.org>
2023-12-15 09:45:24 +0000

    working on error handling

--- api/errors.js
+++ api/errors.js
@@ -1,7 +1,6 @@
 exports.handleCustomErrors = (err, req, res, next) => {
   if (err.status && err.msg) {
-    // res.status(err.status).send({ msg: err.msg });
-    res.status(err.status).send({ msg: err.msg || 'test' });
+    res.status(err.status).send({ msg: err.msg });
   } else {
     next(err);
   }
@@ -11,8 +10,8 @@ exports.handlePSQLErrors = (err, req, res, next) => {
   if (err.code === "22P02" || err.code === "42703" || err.code === "23502") {
     res.status(400).send({ msg: "bad request" });
   } else if (err.code === "23503") {
-    
-    const additionalDetail = err.constraint === 'comments_author_fkey' ? "user" : "article";
+    const additionalDetail =
+      err.constraint === "comments_author_fkey" ? "user" : "article";
 
     res.status(404).send({ msg: `${additionalDetail} not found` });
   } else {
